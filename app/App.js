315e6ceb5cd227b2e005cd07479e6946bed82853
Jason Turner <jasonalmaturner@gmail.com>
2016-01-15 22:10:26 -0700

    i'll finish later

---
+++ app/App.js
@@ -0,0 +1,39 @@
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
+
+const addCounter = (list) => {
+  return [...list, 0];
+};
+
+const removeCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    ...list.slice(index + 1),
+  ];
+};
+
+const testAddCounter = () => {
+  const listBefore = [];
+  const listAfter = [0];
+
+  deepFreeze(listBefore);
+
+  expect(
+    addCounter(listBefore)
+  ).toEqual(listAfter);
+};
+
+const testRemoveCounter = () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 20];
+
+  deepFreeze(listBefore);
+
+  expect(
+    removeCounter(listBefore, 1)
+  ).toEqual(listAfter);
+};
+
+testAddCounter();
+testRemoveCounter();
+console.log('all tests passed')
